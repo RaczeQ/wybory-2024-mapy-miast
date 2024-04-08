35809e532559a9b638f8f9f1f6ac533f3aeda255
Kamil Raczycki <kamil.raczycki@allegro.com>
2024-04-09 01:10:14 +0200

    chore: add option dropdown

--- data_parser/config.py
+++ data_parser/config.py
@@ -3,3 +3,23 @@ ELECTIONS_TYPE = {
     "sejmik": "sejmik_wojewodztwa",
     "rada": "rada_gminy",
 }
+
+geyser_hexes = [
+    "#008080",
+    "#70a494",
+    "#b4c8a8",
+    "#f6edbd",
+    "#edbb8a",
+    "#de8a5a",
+    "#ca562c",
+]
+sunset_hexes = [
+    "#f3e79b",
+    "#fac484",
+    "#f8a07e",
+    "#eb7f86",
+    "#ce6693",
+    "#a059a0",
+    "#5c53a5",
+]
+ICON_URL = "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAYAAAAf8/9hAAAACXBIWXMAAA7EAAAOxAGVKw4bAAABfUlEQVQ4jbXSvUtCURgG8OeVy4XK1LAccjC/hiutpjS6FIG1CSK4ZULQIOnSFhEkNATpEBLkHyBEgy0NLSJGixiE4JgNXun6gUh+nBaJzK8iesfz8vw4POdw+ONw/wowxhaJqPhrQJKkZCGVWs+cnyMTi0G9ugqDIFwQ0c5EoJTL3e26XI7XZhM8x6EriqhHozhwOHyMMXxH+oCyKN7uud2OBYsFl+EwKJuFaLXiQaXCMQAEgwPIJ8AYc+57PGtzej1OwmHwPI9Wb2eVJAQLBRwBWM7nfQAGAQCo1GrYcrvB8/xALyuVCmY6Hbyl0+M7mDiNxnigVq0Ozb3LZGjJZODM5uEAEd1cX11VTyMRhcFohM1m6wsfmkwwTk9jyW5PjrzBptcrUKn0EgoEsO33Y6pcRn1+HvdqNaBU4iwef5LL5RsjASIqMsa00GiebxOJWbTb6AoCtDodQpFIUqFU9oWHdtD7ugrGmPPLcZGIHod1M/IViOhm1O5HwE/nA+wLfijqce7EAAAAAElFTkSuQmCC"
